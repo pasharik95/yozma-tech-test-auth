84a4118d6b0b47882d09a9ca783872f9755a7382
Pavlo Servatovych <pasharikua95@gmail.com>
2020-02-03 23:26:13 +0200

    add not found, error middlewares

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
-
+const {
+  NOT_FOUND,
+  INTERNAL_SERVER_ERROR
+} = require('http-status-codes')
 const usersRouter = require('./routers/users')
 
 const app = express()
@@ -13,6 +16,16 @@ const port = process.env.port || 3000
 app.use(express.json())
 
 app.use(usersRouter)
+
+// Not found middleware
+app.use((req, res) => {
+  res.status(NOT_FOUND).json({ message: 'Not found' })
+})
+
+// Error middleware
+app.use((err, req, res, next) => {
+  res.status(err.status ? err.status : INTERNAL_SERVER_ERROR ).json({ message: err.message })
+})
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`)
